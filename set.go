d335bb241f4afc5ec3c8b013d1fa162627b77016
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2023-01-15 13:51:25 +0900

    add set functions

--- set.go
+++ set.go
@@ -1,20 +1,33 @@
 package goset
 
+import "github.com/samber/lo"
+
+// Sum returns set by sum.
+// expressed by `a or b`.
+func Sum[T any, R comparable](aSlice, bSlice []T, key func(key T) R) []T {
+	flat := lo.Flatten([][]T{aSlice, bSlice})
+	ma := SetBy(flat, key)
+	return lo.Values(ma)
+}
+
+// Intersect returns intersection set expressed by slice.
+// This function expressed by `aSlice and bSlice`.
+func Intersect[T any, R comparable](aSlice, bSlice []T, key func(key T) R) []T {
+	aMap := SetBy(aSlice, key)
+	bMap := SetBy(bSlice, key)
+	diff := make([]T, 0)
+	for ka, va := range aMap {
+		if _, ok := bMap[ka]; ok {
+			diff = append(diff, va)
+		}
+	}
+	return diff
+}
+
+// Difference returns set difference calclated by aSlice - bSlice expressed by slice.
 func Difference[T any, R comparable](aSlice, bSlice []T, key func(key T) R) []T {
-	aMap := make(map[R]T)
-	bMap := make(map[R]T)
-	for _, a := range aSlice {
-		key := key(a)
-		if _, ok := aMap[key]; !ok {
-			aMap[key] = a
-		}
-	}
-	for _, b := range bSlice {
-		key := key(b)
-		if _, ok := bMap[key]; !ok {
-			bMap[key] = b
-		}
-	}
+	aMap := SetBy(aSlice, key)
+	bMap := SetBy(bSlice, key)
 	diff := make([]T, 0)
 	for ka, va := range aMap {
 		if _, ok := bMap[ka]; !ok {
@@ -22,4 +35,16 @@ func Difference[T any, R comparable](aSlice, bSlice []T, key func(key T) R) []T 
 		}
 	}
 	return diff
+}
+
+// SetBy returns map structured by no duplicated elements.
+func SetBy[T any, R comparable](slice []T, key func(key T) R) map[R]T {
+	mapped := make(map[R]T)
+	for _, elem := range slice {
+		key := key(elem)
+		if _, ok := mapped[key]; !ok {
+			mapped[key] = elem
+		}
+	}
+	return mapped
 }
--- set_test.go
+++ set_test.go
@@ -6,12 +6,82 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSum(t *testing.T) {
+	testPatterns := map[string]struct {
+		a      []int
+		b      []int
+		expect []int
+	}{
+		"no elems": {
+			expect: []int{},
+		},
+		"normal": {
+			a:      []int{1, 3, 5},
+			b:      []int{2, 4, 6},
+			expect: []int{1, 2, 3, 4, 5, 6},
+		},
+		"a==b": {
+			a:      []int{1, 2, 3},
+			b:      []int{1, 2, 3},
+			expect: []int{1, 2, 3},
+		},
+		"a is empty": {
+			a:      []int{},
+			b:      []int{1, 2, 3},
+			expect: []int{1, 2, 3},
+		},
+		"b is empty": {
+			a:      []int{1, 2, 3},
+			b:      []int{},
+			expect: []int{1, 2, 3},
+		},
+	}
+	for k, v := range testPatterns {
+		t.Run(k, func(t *testing.T) {
+			actual := Sum(v.a, v.b, func(i int) int { return i })
+			assert.ElementsMatch(t, v.expect, actual)
+		})
+	}
+}
+
+func TestIntersect(t *testing.T) {
+	testPatterns := map[string]struct {
+		a      []int
+		b      []int
+		expect []int
+	}{
+		"no elems": {
+			expect: []int{},
+		},
+		"a==b": {
+			a:      []int{1, 2, 3},
+			b:      []int{1, 2, 3},
+			expect: []int{1, 2, 3},
+		},
+		"odd and even": {
+			a:      []int{1, 3, 5},
+			b:      []int{2, 4, 6},
+			expect: []int{},
+		},
+	}
+	for k, v := range testPatterns {
+		t.Run(k, func(t *testing.T) {
+			actual := Intersect(v.a, v.b, func(item int) int { return item })
+			assert.ElementsMatch(t, v.expect, actual)
+		})
+	}
+}
 func TestDifference(t *testing.T) {
 	testPatterns := map[string]struct {
 		a      []int
 		b      []int
 		expect []int
 	}{
+		"no elems": {
+			a:      []int{},
+			b:      []int{},
+			expect: []int{},
+		},
 		"a==b": {
 			a:      []int{1, 2, 3, 4},
 			b:      []int{1, 2, 3, 4},
@@ -31,7 +101,26 @@ func TestDifference(t *testing.T) {
 	for k, v := range testPatterns {
 		t.Run(k, func(t *testing.T) {
 			actual := Difference(v.a, v.b, func(key int) int { return key })
+			assert.ElementsMatch(t, v.expect, actual)
+		})
+	}
+}
+
+func TestSetBy(t *testing.T) {
+	testPatterns := map[string]struct {
+		input  []int
+		expect map[int]int
+	}{
+		"no elems": {
+			input:  []int{1, 2, 3},
+			expect: map[int]int{1: 1, 2: 2, 3: 3},
+		},
+	}
+	for k, v := range testPatterns {
+		t.Run(k, func(t *testing.T) {
+			actual := SetBy(v.input, func(i int) int { return i })
 			assert.Equal(t, v.expect, actual)
 		})
 	}
+
 }
